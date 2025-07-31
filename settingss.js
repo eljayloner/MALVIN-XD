8cb90fea75f378e33c1c8fa3e1af81bf38e8eab6
lonerj10-hash <ljlj68277@gmail.com>
2025-07-31 11:00:19 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: "malvin~s6En3SpT#wrZlrKq7Hl84rz5YHgZ-bMxwHQqD6wAgxlqaG20SKok ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "2348087205566", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
