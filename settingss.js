e742f293d5e585bf164127ba8ea8940d3614f795
eljayloner <ljloner26@gmail.com>
2025-08-10 00:32:43 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~VrkAmBbQ#WTMfV5wzUFezggRvHPI7FzOMxXsElycR73Xper68Gf8",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "2349031929027", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
